c79fc6ac5cb14a7cc3766efa2f0a68ed18454f62
Dani <78885644+dann-chan@users.noreply.github.com>
2026-09-23 12:26:15 -0400

    Update profile title and avatar in Resume component

--- frontend/src/pages/Profile/Resume.jsx
+++ frontend/src/pages/Profile/Resume.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 const ResumeTab = ({ secretData }) => {
   const profileData = {
     name: "Danny Chan",
-    title: "Senior Technical Product Manager & Solutions Architect",
+    title: "Senior Solutions Architect & Program Manager",
     location: "Toronto, Canada",
-    avatar: "https://1drv.ms",
+    avatar: "https://1drv.ms/i/c/3bb4dc98896970fa/IQQ2hU-U9W3TQo4xcdaV_gBQASbOI9s-lQpb6bMEb2ivV1E",
     bio: "5 years of specialized experience strategizing enterprise AI initiatives, securing major fundings, and gaining high-level executive recognition. Historically notable for designing and deploying the premier AI forecasting infrastructure for Bell's CCaaS. Proven adept at managing capital budgets and leading cross-functional engineering teams to deliver complex, multi-million dollar software projects.",
     
     skills: [ 
