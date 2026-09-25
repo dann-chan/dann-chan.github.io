9a1b9d1e47e265a2cea61f3649eac74bb1aafb3c
Dani <78885644+dann-chan@users.noreply.github.com>
2026-09-24 23:50:51 -0400

    Refactor index.js by cleaning up comments
    Removed comment about booting up the main workspace layer.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 // frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Global global styles if you have any
-import MainApp from './MainApp'; // <-- Imports your actual React workspace root
+import './index.css'; 
+import MainApp from './MainApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* This boots up your main workspace layer */}
     <MainApp /> 
   </React.StrictMode>
 );
